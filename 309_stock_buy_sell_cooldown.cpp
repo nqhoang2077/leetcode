db79d9723f0a72124bb25d6d4a60597c5a4ee0be
nqhoang2077-mairm1 <nqhoang2077@gmail.com>
2022-06-15 10:44:03 +0700

    implemented solution for 309 (from CodeStudio)

--- 309_stock_buy_sell_cooldown.cpp
+++ 309_stock_buy_sell_cooldown.cpp
@@ -10,34 +10,35 @@ using namespace std;
 
 class Solution
 {
+private:
+    vector<vector<int>> dp;
+    int f(vector<int> &prices, int i, int canBuy)
+    {
+        if (i >= prices.size())
+            return 0;
+        if (dp[i][canBuy] != -1)
+            return dp[i][canBuy];
+        int x, y;
+        if (canBuy)
+        {
+            x = f(prices, i + 1, 0) - prices[i];
+            y = f(prices, i + 1, 1);
+        }
+        else
+        {
+            x = f(prices, i + 2, 1) + prices[i];
+            y = f(prices, i + 1, 0);
+        }
+        return dp[i][canBuy] = max(x, y);
+    }
+
 public:
     int maxProfit(vector<int> &prices)
     {
-        auto N{prices.size()};
-        auto &p = prices;
-        if (N < 2)
-            return 0;
-        vector<int> profit(N, 0);
-        int maxProfit{(prices[0] < prices[1]) ? prices[1] - prices[0] : 0};
-        profit[1] = maxProfit;
-
-        for (int i = 2; i < N; i++)
-        {
-            vector<int> comp{
-                profit[i - 1],
-                profit[i - 2],
-                ((i > 2) ? profit[i - 3] : 0) + prices[i] - prices[i - 1],
-                ((i > 2) ? profit[i - 3] : 0) + prices[i] - prices[i - 2]};
-            if (i == N-1)
-                for_each(comp.begin(), comp.end(), [](int p)
-                         { cout << p << ".. "; });
-            profit[i] = *max_element(comp.begin(), comp.end());
-            maxProfit = max(maxProfit, profit[i]);
-        }
-        cout << endl;
-        for_each(profit.begin(), profit.end(), [](int p)
-                 { cout << p << "; "; });
-        return max(0, maxProfit);
+        dp = vector<vector<int>>(prices.size());
+        for (auto &r : dp)
+            r = vector<int>(2, -1);
+        return f(prices, 0, 1);
     }
 };
 
@@ -46,9 +47,9 @@ int main(int argc, char const *argv[])
     Solution s;
     vector<vector<int>> tests{
         // {1, 2, 4},
-        {6,1,3,2,4},
+        {6, 1, 3, 2, 4},
         // {6,1,3,2,4,7}
-        };
+    };
     for (auto &ins : tests)
     {
         for_each(ins.begin(), ins.end(), [](int x)
